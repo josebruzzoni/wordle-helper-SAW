b0a6c74cc84d1118cd5cd505a3f5f07efb2933cb
JulianSima <49444983+JulianSima@users.noreply.github.com>
2022-03-30 15:47:23 -0300

    First commit

---
+++ src/main/java/com/tacs2022/wordlehelper/WordleHelperApplication.java
@@ -0,0 +1,13 @@
+package com.tacs2022.wordlehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordleHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacs2022/wordlehelper/WordleHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacs2022.wordlehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
