78aeeee93026dc7acdea7c8c2b7401b29a5155f6
JulianSima <49444983+JulianSima@users.noreply.github.com>
2022-04-09 16:06:17 -0300

    Change method name in DictionaryService

--- src/main/java/com/tacs2022/wordlehelper/service/DictionaryService.java
+++ src/main/java/com/tacs2022/wordlehelper/service/DictionaryService.java
@@ -1,6 +1,5 @@
 package com.tacs2022.wordlehelper.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.tacs2022.wordlehelper.domain.Word;
@@ -9,13 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DictionaryService {
-    public Word findByName(String name, String language) {
+    public Word findByNameAndLanguage(String name, String language) {
         return new Word("1", name, "definition", language);
     }
 
-    public List<Word> findAllByName(String name, String language) {
-        List<Word> words = new ArrayList<Word>();
-        words.add(new Word("1", name, "definition", language));
-        return words;
+    public List<Word> findAllByNameAndLanguage(String name, String language) {
+        return List.of(new Word("1", name, "definition", language));
     }
 }
