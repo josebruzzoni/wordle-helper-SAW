b4cce9111f5df3e9084aacafbcdadef165546241
MartinezAlexander <alexandercristianmartinez@gmail.com>
2022-04-11 20:20:57 -0300

    add POST /tournaments/:idTournament/participants

--- src/main/java/com/tacs2022/wordlehelper/controller/TournamentController.java
+++ src/main/java/com/tacs2022/wordlehelper/controller/TournamentController.java
@@ -1,12 +1,16 @@
 package com.tacs2022.wordlehelper.controller;
 
+import com.tacs2022.wordlehelper.domain.User;
 import com.tacs2022.wordlehelper.domain.Tournaments.Tournament;
 import com.tacs2022.wordlehelper.service.TournamentService;
 import com.tacs2022.wordlehelper.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TournamentController {
@@ -30,15 +34,16 @@ public class TournamentController {
         return tournamentService.getLeaderboardOfTournament(id);
     }
 
-    /*
-    @PostMapping("/tournaments/{id}/participants")
-    public void addParticipant(@RequestBody JSONObject jsonRequest, @PathVariable(value = "id") Long idTournament){
+	@PostMapping("/tournaments/{id}/participants")
+    public ResponseEntity<Void> addParticipant(@RequestBody Map<String, Object> json, @PathVariable(value = "id") Long idTournament){
 
-        Long id_long = ((Number) jsonRequest.get("participant_id")).longValue();
-        User newParticipant = userService.findById(jsonRequest.);
+        Integer id = (Integer) json.getOrDefault("idParticipant", Integer.valueOf(1));
+        User newParticipant = userService.findById(id.longValue());
         Tournament tournament = tournamentService.findById(idTournament);
         tournament.addParticipant(newParticipant);
-    }*/
+        
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
 
     @PostMapping("/tournaments")
     public Tournament postTournament(@RequestBody Tournament tournament){
