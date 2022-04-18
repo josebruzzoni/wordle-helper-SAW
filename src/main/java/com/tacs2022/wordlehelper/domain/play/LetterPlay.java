e3f30028d5fd408deb25e0914f3583567c0a776d
Agustin Cragno <cragno.agustin@gmail.com>
2022-04-18 16:37:36 -0300

    Changed helper path to receive attempted play through query params

--- src/main/java/com/tacs2022/wordlehelper/domain/play/LetterPlay.java
+++ src/main/java/com/tacs2022/wordlehelper/domain/play/LetterPlay.java
@@ -1,8 +1,10 @@
 package com.tacs2022.wordlehelper.domain.play;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LetterPlay {
 
     private Character letter;
