2a7219f7af31b127d0d5871529c242e3984d9c5a
josebruzzoni <bruzzonijose@gmail.com>
2022-04-20 23:47:20 -0300

    dtos helper

--- src/main/java/com/tacs2022/wordlehelper/controller/HelperController.java
+++ src/main/java/com/tacs2022/wordlehelper/controller/HelperController.java
@@ -1,6 +1,7 @@
 package com.tacs2022.wordlehelper.controller;
 
 import com.tacs2022.wordlehelper.domain.play.WordPlay;
+import com.tacs2022.wordlehelper.dtos.helper.OutputPossibleWordsDto;
 import com.tacs2022.wordlehelper.service.HelperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +17,12 @@ public class HelperController {
     private HelperService helperService;
 
     @GetMapping("/words")
-    public Map<String, List<String>> getPossibleWords(@RequestParam(value = "grey") String greyLettersPlayed,
+    public OutputPossibleWordsDto getPossibleWords(@RequestParam(value = "grey") String greyLettersPlayed,
                                                       @RequestParam(value = "yellow") String yellowLettersPlayed,
                                                       @RequestParam(value = "green") String greenLettersPlayed) {
         //TODO: verify correct format of params => yellow and green must be five characters long, with only letters and '_'
         WordPlay attemptedPlay = new WordPlay(greyLettersPlayed, yellowLettersPlayed, greenLettersPlayed);
         List<String> possibleWords = helperService.getWordsByPlay(attemptedPlay);
-        Map<String, List<String>> response = new HashMap<>();
-        response.put("possibleWords", possibleWords);
-        return response;
+        return new OutputPossibleWordsDto(possibleWords);
     }
 }
