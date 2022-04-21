0235dabcb791d0782f82f2681967ed9b79dbe506
kvothe838 <facundoyuffrida@gmail.com>
2022-04-21 21:54:35 +0200

    Bot devuelve botón para loguearse

--- src/main/java/com/tacs2022/wordlehelper/controller/TelegramController.java
+++ src/main/java/com/tacs2022/wordlehelper/controller/TelegramController.java
@@ -2,7 +2,11 @@ package com.tacs2022.wordlehelper.controller;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class TelegramController {
@@ -12,11 +16,16 @@ public class TelegramController {
         TelegramBot bot = new TelegramBot(key);
 
         bot.setUpdatesListener(updates -> {
-            // ... process updates
-            // return id of last processed update or confirm them all
             System.out.println(updates);
             long chatId = updates.get(0).message().chat().id();
-            bot.execute(new SendMessage(chatId, "Hello!"));
+            InlineKeyboardButton loginButton = new InlineKeyboardButton("Loguearse").callbackData("login");
+            InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup(loginButton);
+            String message = "Seleccionar acción a realizar";
+            SendMessage sendMessage = new SendMessage(chatId, message).replyMarkup(keyboardMarkup);
+            SendResponse sendResponse = bot.execute(sendMessage);
+            /*System.out.println(sendResponse.isOk());
+            System.out.println(sendResponse.errorCode());
+            System.out.println(sendResponse.description());*/
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
     }
