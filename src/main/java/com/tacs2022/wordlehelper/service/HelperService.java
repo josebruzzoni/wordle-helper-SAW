b05d7a2c25efa142f0c7cf550b04953a79a2de20
Agustin Cragno <cragno.agustin@gmail.com>
2022-05-05 22:37:59 -0300

    Added javadoc to helper methods

--- src/main/java/com/tacs2022/wordlehelper/service/HelperService.java
+++ src/main/java/com/tacs2022/wordlehelper/service/HelperService.java
@@ -25,6 +25,13 @@ public class HelperService {
             Language.ES, "src/main/resources/helper/5letter-spanish.list"
     );
 
+    /**
+     * Generates a list of possible words for the given word play in the given language.
+     *
+     * @param wordPlay Attempted play
+     * @param language Language the game was played in
+     * @return Possible winning words
+     */
     public List<String> getWordsByPlay(WordPlay wordPlay, Language language){
 
         List<String> possibleOptions = new ArrayList<>();
@@ -52,7 +59,14 @@ public class HelperService {
     //Regular Expressions doc
     //https://www.javatpoint.com/java-regex#:~:text=The%20Java%20Regex%20or%20Regular,the%20Java%20Regex%20Tester%20Tool.
 
-    public String buildAllInPositionRegularExpression(List<LetterPlay> lettersPlayed){
+    /**
+     * Builds a regex string that represents words that contain all the given letters in their
+     * respective positions
+     *
+     * @param lettersPlayed List of letters to build the regex from
+     * @return 'All In Position' regular expression
+     */
+    private String buildAllInPositionRegularExpression(List<LetterPlay> lettersPlayed){
         StringBuilder regex = new StringBuilder();
         for (int i = 0 ; i < WORD_LENGTH ; i++){
             regex.append(getCharacterAtPositionIfExists(lettersPlayed, i));
@@ -60,14 +74,20 @@ public class HelperService {
         return regex.toString();
     }
 
-    public Character getCharacterAtPositionIfExists(List<LetterPlay> lettersPlayed, Integer p){
+    private Character getCharacterAtPositionIfExists(List<LetterPlay> lettersPlayed, Integer p){
         for (LetterPlay letterPlay : lettersPlayed){
             if (letterPlay.getPosition().equals(p)) return letterPlay.getLetter();
         }
         return '.';//Any character for regular expressions
     }
 
-    public String buildNoneOfRegularExpression(List<LetterPlay> lettersPlayed){
+    /**
+     * Builds a regex string that represents words that contain none of the given words in any position.
+     *
+     * @param lettersPlayed List of letters to build the regex from
+     * @return 'None Of' regular expression
+     */
+    private String buildNoneOfRegularExpression(List<LetterPlay> lettersPlayed){
         //string of all characters together
         String letters = lettersPlayed.stream().map(l -> l.getLetter().toString()).reduce((acc, e) -> acc  + e).orElse(".");
         //regex that represents if a character doesn't match any of letters
@@ -76,7 +96,15 @@ public class HelperService {
         return notAnyLetterRegex.repeat(WORD_LENGTH);
     }
 
-    public boolean hasEveryOfNotInPosition(String word, List<LetterPlay> lettersPlayed){
+    /**
+     * Checks if the given word contains all the letters played in a position different from
+     * the given one.
+     *
+     * @param word Word to test
+     * @param lettersPlayed List of letters
+     * @return true if word contains all letters but in another position, false if at least one letter is in the given position or one letter is not in the string
+     */
+    private boolean hasEveryOfNotInPosition(String word, List<LetterPlay> lettersPlayed){
         for (LetterPlay letter : lettersPlayed){
             if (!word.contains(letter.getLetter().toString()) //word does not contain letter
                     || (word.contains(letter.getLetter().toString()) && word.charAt(letter.getPosition()) == letter.getLetter())) //word contains letter but in rejected position
