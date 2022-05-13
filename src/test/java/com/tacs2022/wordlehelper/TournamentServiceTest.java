79458d0ce14986b27c829c7ceedfa9854d1c6009
MartinezAlexander <alexandercristianmartinez@gmail.com>
2022-05-13 01:09:45 -0300

    testing - add participant

--- src/test/java/com/tacs2022/wordlehelper/TournamentServiceTest.java
+++ src/test/java/com/tacs2022/wordlehelper/TournamentServiceTest.java
@@ -1,6 +1,9 @@
 package com.tacs2022.wordlehelper;
 
 import static java.time.LocalDate.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
@@ -12,6 +15,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +24,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.tacs2022.wordlehelper.domain.Language;
 import com.tacs2022.wordlehelper.domain.tournaments.Tournament;
+import com.tacs2022.wordlehelper.domain.tournaments.TournamentStatus;
 import com.tacs2022.wordlehelper.domain.tournaments.Visibility;
 import com.tacs2022.wordlehelper.domain.user.User;
 import com.tacs2022.wordlehelper.exceptions.ForbiddenException;
@@ -34,12 +39,14 @@ public class TournamentServiceTest {
 	@MockBean
 	TournamentRepository tournamentRepoMock;
 	
+	@Mock
+	Tournament tournamentMock;
+	
 	@Autowired
 	TournamentService tournamentService;
 	
-	Tournament tournament;
-	LocalDate startDate = of(2016, 6, 10);
-	LocalDate endDate = of(2016, 6, 12);
+	Tournament privateTournament;
+	Tournament publicTournament;
 	
 	User julian;
 	User agus;
@@ -50,19 +57,76 @@ public class TournamentServiceTest {
 		byte[] salt = ss.getSalt();
 
 		julian = new User("Julian", ss.hash("1234", salt), salt);
-		tournament = new Tournament("Luchemos por la vida", startDate, endDate,
+		privateTournament = new Tournament("Superliga", LocalDate.now().plusWeeks(1), LocalDate.now().plusWeeks(2),
 				Visibility.PRIVATE, List.of(Language.EN, Language.ES), julian);
+		publicTournament = new Tournament("Ligue 1", LocalDate.now().plusWeeks(1), LocalDate.now().plusWeeks(2),
+				Visibility.PUBLIC, List.of(Language.EN, Language.ES), julian);
 		
 		agus = new User("Julian", ss.hash("1234", salt), salt);
 	}
 	
 	@Test
-	public void aTest() {
-		Mockito.when(tournamentRepoMock.findById(tournament.getId())).thenReturn(Optional.of(tournament));
-		Assertions.assertThatThrownBy(() -> {
-			tournamentService.addParticipant(tournament.getId(), julian, agus);
-		})
-		.isInstanceOf(ForbiddenException.class)
-		.hasMessage("Participants cannot be added to this tournament once it has started or finished");
+	public void youCannotAddParticipantsToTournamentsThatHaveAStartedStatus() {
+		Mockito.when(tournamentRepoMock.findById(anyLong())).thenReturn(Optional.of(tournamentMock));
+		Mockito.when(tournamentMock.getStatus()).thenReturn(TournamentStatus.STARTED);
+		Assertions
+			.assertThatThrownBy ( () -> { tournamentService.addParticipant(Long.valueOf(1), julian, agus); } )
+			.isInstanceOf(ForbiddenException.class)
+			.hasMessage("Participants cannot be added to this tournament once it has started or finished");
+	}
+	
+	@Test
+	public void youCannotAddParticipantsToTournamentsThatHaveAFinishedStatus() {
+		Mockito.when(tournamentRepoMock.findById(anyLong())).thenReturn(Optional.of(tournamentMock));
+		Mockito.when(tournamentMock.getStatus()).thenReturn(TournamentStatus.FINISHED);
+		Assertions
+			.assertThatThrownBy ( () -> { tournamentService.addParticipant(Long.valueOf(1), julian, agus); } )
+			.isInstanceOf(ForbiddenException.class)
+			.hasMessage("Participants cannot be added to this tournament once it has started or finished");
+	}
+	
+	@Test
+	public void userTriedToAddParticipantToPrivateTournamentWithoutBeingTheOwner() {
+		Mockito.when(tournamentRepoMock.findById(anyLong())).thenReturn(Optional.of(tournamentMock));
+		Mockito.when(tournamentMock.getStatus()).thenReturn(TournamentStatus.NOTSTARTED);
+		Mockito.when(tournamentMock.getVisibility()).thenReturn(Visibility.PRIVATE);
+		Mockito.when(tournamentMock.userIsOwner(any(User.class))).thenReturn(false);
+		Assertions
+			.assertThatThrownBy ( () -> { tournamentService.addParticipant(Long.valueOf(1), julian, agus); } )
+			.isInstanceOf(ForbiddenException.class)
+			.hasMessage("User cannot add participant to this private tournament without being the owner");
+	}
+	
+	/*@Test
+	public void userTriedToAddParticipantToPublicTournamentWithoutBeingTheOwnerCanOnlyAddSelf() {
+		Mockito.when(tournamentRepoMock.findById(anyLong())).thenReturn(Optional.of(tournamentMock));
+		Mockito.when(tournamentMock.getStatus()).thenReturn(TournamentStatus.NOTSTARTED);
+		Mockito.when(tournamentMock.getVisibility()).thenReturn(Visibility.PUBLIC);
+		Mockito.when(tournamentMock.userIsOwner(any(User.class))).thenReturn(false);
+		Assertions
+			.assertThatThrownBy ( () -> { tournamentService.addParticipant(Long.valueOf(1), julian, agus); } )
+			.isInstanceOf(ForbiddenException.class)
+			.hasMessage("User cannot add participant to this private tournament without being the owner");
+	}*/
+	
+	@Test
+	public void AsAUserIWantToBeAbleToAddAnotherUserToAPrivateTournamentThatICreated() {
+		Mockito.when(tournamentRepoMock.findById(anyLong())).thenReturn(Optional.of(privateTournament));
+		Assertions.assertThatNoException()
+			.isThrownBy(() -> { tournamentService.addParticipant(Long.valueOf(1), julian, agus); });
+	}
+	
+	@Test 
+	public void asAUserIWantToBeAbleToAddAnotherUserToAPublicTournamentThatICreated() {
+		Mockito.when(tournamentRepoMock.findById(anyLong())).thenReturn(Optional.of(publicTournament));
+		Assertions.assertThatNoException()
+			.isThrownBy(() -> { tournamentService.addParticipant(Long.valueOf(1), julian, agus); });
+	}
+	
+	@Test
+	public void asAUserIWantToBeAbleToJoinAPublicTournamentThatHasNotStartedYet(){
+		Mockito.when(tournamentRepoMock.findById(anyLong())).thenReturn(Optional.of(publicTournament));
+		Assertions.assertThatNoException()
+			.isThrownBy(() -> { tournamentService.addParticipant(Long.valueOf(1), agus, agus); });
 	}
 }
