7a1e10661c0299fc3ef93f5b3f663f9f1507f128
JulianSima <49444983+JulianSima@users.noreply.github.com>
2022-10-24 20:52:36 -0300

    h2

--- src/main/java/com/tacs2022/wordlehelper/controller/AdminController.java
+++ src/main/java/com/tacs2022/wordlehelper/controller/AdminController.java
@@ -1,16 +1,28 @@
 package com.tacs2022.wordlehelper.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/admin")
 public class AdminController {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public String admin(@RequestBody String body) {
-        System.out.println(body);
+        jdbcTemplate.execute("SELECT * FROM table WHERE algo=" + body);
         return body;
+    }
+
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public String getAlgo() {
+        jdbcTemplate.execute("SELECT * FROM table WHERE algo=");
+        return "";
     }
 }
